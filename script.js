9d10f962e1ad6d6fdaa8e5d18ca8eeda45805c96
Banashree Aribenchi <banashree.aribenchi17@gmail.com>
2023-11-03 08:19:45 +0100

    complete basic puzzle

--- script.js
+++ script.js
@@ -14,33 +14,32 @@ class Board {
   }
 }
 
-let canvas = document.getElementById("canvas"); //gets a reference to the HTML canvas element with the ID "canvas" and stores it in the canvas variable.
-let ctx = canvas.getContext("2d"); //retrieves the 2D rendering context of the canvas, which allows you to draw graphics on it.
+let canvas = document.getElementById("canvas"); //ref to canvas element and stores it in the canvas variable.
+let ctx = canvas.getContext("2d"); //retrieves the 2D rendering context of the canvas
 let board;
 let tileImgs = [];
-let tileIds = [];
+let tileIds = []; //original order of tiles
 let shuffledIds = [];
-let img = new Image(); //creates a new Image object that will be used to load the puzzle image.
-img.src = "./dogs.png"; //Sets the src attribute of the img object to the path of the puzzle image you want to use.
+let img = new Image(); //creates a new Image object
+img.src = "./dogs.png";
 img.onload = () => {
-  //is an event handler that runs when the image has finished loading. It's a callback function
-  console.log("Image loaded successfully");
+  //event handler
+  //console.log("Image loaded successfully");
   cutImageIntoPieces.call(img);
   canvas.addEventListener("click", move);
-  shuffle();
 };
 
 function cutImageIntoPieces() {
-  board = new Board(this.width, this.height, 3); //creates a new Board object using the dimensions of the loaded image and sets the number of rows and columns to 3.
-  canvas.width = board.width; //set to match the dimensions of the board.
+  board = new Board(this.width, this.height, 3); //creates a new Board object
+  canvas.width = board.width; //set to match the dimensions of the board
   canvas.height = board.height;
   canvas.style.position = "absolute";
-  canvas.style.border = "red 2px solid";
+  canvas.style.border = "beige 2px solid";
   //   ctx.fillStyle = "gray";
   //   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   for (let row = 0; row < board.rowCols; row++) {
-    //loop iterates over the rows and columns to cut the image into pieces and store them in tileImgs
+    //loop iterates over the rows and columns to cut the image into pieces
     for (let col = 0; col < board.rowCols; col++) {
       let tmpCanvas = document.createElement("canvas");
       tmpCanvas.width = board.tileWidth;
@@ -64,41 +63,20 @@ function cutImageIntoPieces() {
           tmpCanvas.width,
           tmpCanvas.height
         );
-        tileImgs.push(tmpCanvas.toDataURL());
+        tileImgs.push(tmpCanvas.toDataURL()); //converts the content of the canvas (image) into data URL
         let id = row + col * board.rowCols;
-        tileIds.push(id);
+
+        tileIds.push(id); //data URLs are pushed into tileIds array
       }
-      //   if (row === 2 && col === 2) {
-      //     // This is the blank tile
-      //     tmpCtx.fillStyle = "gray";
-      //     tmpCtx.fillRect(0, 0, board.tileWidth, board.tileHeight);
-      //     tileImgs.push(tmpCanvas.toDataURL());
-      //     console.log("tile images>", tileImgs);
-      //     tileIds.push(-1); // Use -1 to represent the blank tile
-      //   } else {
-      //     tmpCtx.drawImage(
-      //       this,
-      //       row * board.widthIP,
-      //       col * board.heightIP,
-      //       board.widthIP,
-      //       board.heightIP,
-      //       0,
-      //       0,
-      //       tmpCanvas.width,
-      //       tmpCanvas.height
-      //     );
-      //     tileImgs.push(tmpCanvas.toDataURL());
-      //     let id = row + col * board.rowCols;
-      //     tileIds.push(id);
-      //   }
     }
   }
   shuffle();
-  console.log("Shuffling is complete. Calling drawAllTiles now.");
+  // console.log("Shuffling is complete. Calling drawAllTiles now.");
   drawAllTiles();
 }
 
 function shuffle() {
+  //shuffles the puzzle pieces until solved
   do {
     shuffledIds = [...tileIds];
     for (let i = shuffledIds.length - 1; i >= 0; i--) {
@@ -110,6 +88,7 @@ function shuffle() {
 }
 
 function isSolvable() {
+  //checks if the current arrangement of puzzle pieces is solvable
   let inversions = 0;
   for (let i = 0; i < shuffledIds.length - 1; i++) {
     for (let j = i + 1; j < shuffledIds.length; j++) {
@@ -126,10 +105,11 @@ function isSolvable() {
 }
 
 function drawAllTiles() {
-  //uses a loop to iterate over the shuffled tile IDs and draw each tile in its corresponding position.
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  let loadedImages = 0;
+  //uses a loop to iterate over the shuffled tile IDs
+  ctx.clearRect(0, 0, canvas.width, canvas.height); //clears the entire canvas
+  let loadedImages = 0; //number of puzzle pieces that have been successfully loaded and drawn
   for (let index = 0; index < shuffledIds.length; index++) {
+    //loop that iterates over the shuffled tile IDs
     if (shuffledIds[index] == -1) continue;
     let coord = getRowColFromIndex(index); //converts a single index into the corresponding row and column.
     let x = coord.x * board.tileWidth;
@@ -175,28 +155,32 @@ function move(e) {
   if (!hasBlankNeighbour(tileX, tileY, blankX, blankY)) return;
   // store pixels of tile with image into temp variable
   const swapDataImage = ctx.getImageData(
-    tileX * board.tileWidth,
+    //captures the image data of the clicked tile
+    tileX * board.tileWidth, //x-axis coordinate of the top-left corner of the tile
     tileY * board.tileHeight,
-    board.tileWidth,
+    board.tileWidth, //width of the tile
     board.tileHeight
   );
   ctx.fillRect(
+    //clears the space occupied by the clicked tile
     tileX * board.tileWidth,
     tileY * board.tileHeight,
     board.tileWidth,
     board.tileHeight
   );
   ctx.putImageData(
+    //pastes the image data of the clicked tile onto the position of the blank tile.
     swapDataImage,
     blankX * board.tileWidth,
     blankY * board.tileHeight
   );
 
-  const imgIdx = getIndexFromCoords(tileX, tileY);
+  const imgIdx = getIndexFromCoords(tileX, tileY); //represents the new position of the clicked tile
   const blankIdx = getIndexFromCoords(blankX, blankY);
 
   swapIndex(imgIdx, blankIdx); //logic to check if puzzle is solved or not
   if (isSolved()) {
+    //new index for the blank tile after the swap
     canvas.removeEventListener("click", move);
     drawLastTile();
     setTimeout(() => alert("Congratulations!!!!!"), 0);
@@ -204,8 +188,8 @@ function move(e) {
 }
 
 function getMouseCoords(x, y) {
-  let offset = canvas.getBoundingClientRect();
-  let left = Math.floor(offset.left);
+  let offset = canvas.getBoundingClientRect(); //returns object about size of element and its position relative
+  let left = Math.floor(offset.left); //distance from the left edge of the viewport to the left edge of the canvas
   let top = Math.floor(offset.top);
   let row = Math.floor((x - left) / board.tileWidth);
   let col = Math.floor((y - top) / board.tileHeight);
@@ -213,11 +197,17 @@ function getMouseCoords(x, y) {
 }
 
 function findBlankIndex() {
-  for (let i = 0; i < shuffledIds.length; i++)
+  //searches for index of blank tile
+  for (
+    let i = 0;
+    i < shuffledIds.length;
+    i++ //iterates through the elements of the shuffledIds array
+  )
     if (shuffledIds[i] == -1) return i;
 }
 
 function hasBlankNeighbour(tileX, tileY, blankX, blankY) {
+  // checks if neighbor is a blank tile
   if (tileX != blankX && tileY != blankY) return false;
   if (Math.abs(tileX - blankX) == 1 || Math.abs(tileY - blankY) == 1)
     return true;
@@ -229,7 +219,7 @@ function getIndexFromCoords(x, y) {
 }
 
 function swapIndex(imgIdx, blankIdx) {
-  shuffledIds[blankIdx] = shuffledIds[imgIdx];
+  shuffledIds[blankIdx] = shuffledIds[imgIdx]; //swaps the positions of the clicked tile and the blank tile
   shuffledIds[imgIdx] = -1;
 }
 
@@ -243,11 +233,11 @@ function isSolved() {
 
 function drawLastTile() {
   let blank = findBlankIndex();
-  let coords = getRowColFromIndex(blank);
-  let x = coords.x;
+  let coords = getRowColFromIndex(blank); //converts 1D index of blank tile into its corresponding 2D coordinates
+  let x = coords.x; //extract the row (x) and column (y) coordinates from the coords object
   let y = coords.y;
-  let imgURL = tileImgs[tileIds[blank]];
-  const imgObj = new Image();
+  let imgURL = tileImgs[tileIds[blank]]; //retrieves the URL of the image for the blank tile from the tileImgs array
+  const imgObj = new Image(); //objectused to load the image of the blank tile
   imgObj.onload = function () {
     ctx.drawImage(
       this,
